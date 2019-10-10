3b0996683c656a22d55eab9085db35280984cc4f
Mathieu <raisinmathieu@mndev.fr>
2019-10-10 16:58:37 +0200

    Debut du projet. Ajours des compensant Map, et Marker.

---
+++ src/Map.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+import MarkerVelo from './Marker'
+import './Map.css';
+
+class MapVelo extends Component{
+  state = {
+    lat: 45.763611683929796,
+    lng: 4.874637805443967,
+    zoom: 13,
+  }
+  render(){
+    const position = [this.state.lat, this.state.lng, this.state.zoom]
+    return(
+      <Map className="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MarkerVelo position={position}/>
+      </Map>
+      
+    )
+  }
+
+}
+export default MapVelo
+
