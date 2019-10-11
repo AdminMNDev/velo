ec2a70959bd3cfd0e4a277c86f812c91f6c3751b
Mathieu <raisinmathieu@mndev.fr>
2019-10-11 16:05:34 +0200

    ajouts de mofification CSS pour la mise en forme du Popup

--- src/Popup.js
+++ src/Popup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Popup } from 'react-leaflet'
+import "./popup.css"
 
 class PopupVelo extends Component{
 
@@ -11,8 +12,21 @@ class PopupVelo extends Component{
                 <h3>{station.name}</h3>
                 <p>{station.address}</p>
                     <div>
-                        <p>Il y a {station.available_bikes} vélo disponible sur {station.bike_stands} -
-                        Il y a {station.available_bike_stands} place disponible sur {station.bike_stands}</p>
+                        <div className="avl_bikes">
+                            <img src="bicycle.png" alt="velo"></img>
+                            <p className="text_avl_bikes">
+                                <p className="first_text_popup">{station.available_bikes}</p><p className="second_text_popup">{station.bike_stands}</p>
+                            </p>
+                            <p className="last_text_popup">Vélos libres</p>
+                        </div>
+                        <div className="avl_bikes">
+                            <img src="parking-sign.png" alt="velo"></img>
+                            <p className="text_avl_bikes">
+                                <p className="first_text_popup">{station.available_bike_stands}</p><p className="second_text_popup">{station.bike_stands}</p>
+                            </p>
+                            <p className="last_text_popup">Places libres</p>
+                        </div>
+                        <button>Reserver !</button>
                     </div>
             </div>
         </Popup>
