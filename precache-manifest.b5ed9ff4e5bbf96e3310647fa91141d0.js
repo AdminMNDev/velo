112c44791ec12192c9ffb41939b2fa88bc1de0b0
Mathieu <raisinmathieu@mndev.fr>
2019-10-18 11:47:29 +0200

    Updates

---
+++ precache-manifest.b5ed9ff4e5bbf96e3310647fa91141d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fbb9a211350acfc929a43457a463d8",
+    "url": "/velo/index.html"
+  },
+  {
+    "revision": "9d4566c5c637bcaf17cf",
+    "url": "/velo/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "a894c33f06584144372a",
+    "url": "/velo/static/css/main.f2c1a82c.chunk.css"
+  },
+  {
+    "revision": "9d4566c5c637bcaf17cf",
+    "url": "/velo/static/js/2.3851afa7.chunk.js"
+  },
+  {
+    "revision": "a894c33f06584144372a",
+    "url": "/velo/static/js/main.c16e52e5.chunk.js"
+  },
+  {
+    "revision": "f38d61703b2283f088a6",
+    "url": "/velo/static/js/runtime-main.f37de9a2.js"
+  },
+  {
+    "revision": "7758feb8a193d5cf97c97d25dc9ba4e0",
+    "url": "/velo/static/media/header.7758feb8.png"
+  }
+]);
